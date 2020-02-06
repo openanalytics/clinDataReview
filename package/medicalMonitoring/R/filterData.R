ac4ba57968ffb0e36c0c780745e2d2b1402a14d9
lcougnaud <laure.cougnaud@openanalytics.eu>
2020-02-06 15:27:05 +0100

    filterData: keepNA + message for missing values

--- package/medicalMonitoring/R/filterData.R
+++ package/medicalMonitoring/R/filterData.R
@@ -1,23 +1,35 @@
 #' Filter dataset based on specified filters.
+#' 
+#' By default, missing values in filtering variable are retained,
+#' see \code{keepNA} parameter.
 #' @param data Data.frame with data.
 #' @param filters Unique filter or list of filters.
 #' Each filter should be a list containing:
 #' \itemize{
 #' \item{'var': }{String with variable to filter on.}
 #' \item{'value': }{Character vector with values from \code{var} to consider.}
-#' \item{'op': }{String with operator used to retain records from \code{value}.
+#' \item{'op': }{(optional) String with operator used to retain records from \code{value}.
 #' If not specified, the inclusion operator: '\%in\%' is considered, a.k.a
 #' records with \code{var} in \code{value} are retained.}
-#' \item{'rev': }{Logical, if TRUE (FALSE by default), the condition specified
+#' \item{'rev': }{(optional) Logical, if TRUE (FALSE by default), the condition specified
 #' in the filter is reverted.}
+#' \item{'keepNA': }{(optional) Logical, if TRUE (by default), missing values in \code{var}
+#' are retained. If not specified, \code{keepNA} general parameter is used.}
 #' }
+#' @param keepNA Logical, if TRUE (by default) missing values in \code{var} are retained.
+#' If set to FALSE, missing values are ignored for all filters.
+#' The specification within \code{filters} prevails on this parameter.
 #' @param verbose Logical, if TRUE (FALSE by default) progress messages are printed
 #' in the current console.
 #' @return Filtered \code{data}.
 #' @example inst/examples/filterData-example.R
 #' @author Laure Cougnaud
 #' @export
-filterData <- function(data, filters, verbose = FALSE){
+filterData <- function(
+	data, 
+	filters, 
+	verbose = FALSE, 
+	keepNA = TRUE){
 	
 	# if multiple filters are specified:
 	# data is successively filtered
@@ -53,16 +65,35 @@ filterData <- function(data, filters, verbose = FALSE){
 	isKept <- match.fun(op)(x = data[, var], table = value)
 	if(rev)	isKept <- !isKept
 	
+	# keep missing values?
+	keepNAFilter <- filters$keepNA
+	if(is.null(keepNAFilter))	keepNAFilter <- keepNA
+	isNA <- is.na(data[, var])
+	if(keepNAFilter){
+		isKept <- isKept | isNA	
+	}
+	
 	idxKept <- which(isKept)
 	
 	if(length(idxKept) == 0)
 		warning("No data is retained based on the filtering on the variable")
 	
-	if(verbose)
-		message(paste(sum(!isKept), "records with", varST, 
-			if(rev)	"not",
-			op, valueST, "are filtered.")
-		)
+	if(verbose){
+		msgNA <- paste(sum(isNA), "records with missing", varST)
+		message(paste0(
+			sum(!isKept), " records with ", varST, 
+			if(rev)	" not", " ",
+			op, " ", valueST, " are filtered",
+			ifelse(
+				sum(isNA) > 0 & all(!is.na(value)),
+				ifelse(keepNAFilter, 
+					paste(".", msgNA, " are retained."), 
+					paste(" (including", msgNA, ").")
+				),
+				"."
+			)
+		))
+	}
 	
 	# filter data
 	data <- data[idxKept, ]
--- package/medicalMonitoring/inst/examples/filterData-example.R
+++ package/medicalMonitoring/inst/examples/filterData-example.R
@@ -13,6 +13,17 @@ filterData(dataDM, filters = list(var = "SEX", value = "M", rev = TRUE), verbose
 # filter based on inequality operator
 filterData(dataDM, filters = list(var = "AGE", value = 20, op = "<="), verbose = TRUE)
 
+# missing values are retained by default!
+dataDMNA <- dataDM
+dataDMNA[1:2, "AGE"] <- NA
+filterData(dataDMNA, filters = list(var = "AGE", value = 20, op = "<="), verbose = TRUE)
+
+# filter missing values on variable
+filterData(dataDMNA, filters = list(var = "AGE", value = 20, op = "<=", keepNA = FALSE), verbose = TRUE)
+
+# retain only missing values
+filterData(dataDMNA, filters = list(var = "AGE", value = NA, keepNA = FALSE), verbose = TRUE)
+
 # multiple filters
 filterData(
 	data = dataDM, 
