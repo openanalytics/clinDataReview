241dc9a4750c5db0682a8a83b615b53a319dca7c
Michela Pasetto <mpasetto@openanalytics.eu>
2020-05-06 18:49:17 +0200

    reorder column function ongoing

--- package/medicalMonitoring/R/compareTables.R
+++ package/medicalMonitoring/R/compareTables.R
@@ -12,7 +12,10 @@
 #' The \code{changeableVars} need to have the same name in both the new and old data sets.
 #' @param labelVars named character vector for renaming the columns of the output table.
 #' If NULL, the column names of the data frame are used.
-#' @param patientProfilePath
+#' @param patientProfilePath string indicating the path to the directory
+#' where the patient profiles are stored.
+#' @param subjectIDvar string indicating which column in the data represents the unique subject identifier.
+#' It is "USUBJID" by default.
 #' @return Return an interactive table showing the changes.
 #' Additions and removals are green or red colored, respectively.
 #' @author Michela Pasetto
@@ -25,7 +28,8 @@ compareTables <- function(
 		referenceVars = NULL,
 		changeableVars = NULL,
 		labelVars = NULL,
-		patientProfilePath = NULL
+		patientProfilePath = NULL,
+		subjectIDvar = "USUBJID"
 ) {
 	
 	if(length(referenceVars) == 0) stop("Provide variables for the comparison, 'referenceVars' is empty.")
@@ -37,8 +41,13 @@ compareTables <- function(
 	if(is.null(labelVars)) {		
 		warning("No 'labelVars' specified. The 'referenceVars' and 'changeableVars' names will be used instead.")
 		labelVars <- varsToUse
-	} else {
-		labelVars <- labelVars
+	} else labelVars <- labelVars
+	
+	if(! subjectIDvar %in% colnames(newData)) {
+		stop(sprintf("Unique subject identifier '%s' not available in the new data", subjectIDvar))
+	}
+	if(! subjectIDvar %in% colnames(oldData)) {
+		stop(sprintf("Unique subject identifier '%s' not available in the old data", subjectIDvar))
 	}
 	
 	# Extract data
@@ -54,7 +63,8 @@ compareTables <- function(
 	comparisonTable <- formatComparisonDF(
 			comparisonDF,
 			labelVars = labelVars,
-			patientProfilePath = patientProfilePath
+			patientProfilePath = patientProfilePath,
+			subjectIDvar = subjectIDvar
 	)
 	
 	notDisplayVars <- colnames(comparisonTable)[grepl("diff", colnames(comparisonTable))]
@@ -125,66 +135,39 @@ getComparisonDF <- function(newData, oldData, referenceVars) {
 #' @return A data frame formated to be used for the interactive table in \code{compareTables}.
 #' The 'Type' column indicates what kind of change has occured in the data whereas
 #' the 'Version' column indicates whether the change happens in the old (Previous) or new (Current) data.
+#' @importFrom glpgUtilityFct getLabelVar
 #' @author Michela Pasetto
 #' @export 
-formatComparisonDF <- function(comparisonDF, labelVars, patientProfilePath) {
+formatComparisonDF <- function(
+		comparisonDF,
+		labelVars,
+		patientProfilePath,
+		subjectIDvar
+) {
 	
 	comparisonDF$Type <- gsub("[+]", "Addition", gsub("=", "Change", gsub("[-]", "Removal", comparisonDF$grp_diff)))	
 	comparisonDF$Version <- gsub("[+]", "Current", gsub("[-]", "Previous", comparisonDF$chng_type))
 	
-	comparisonDF$patientProfilePath <- paste0(
-			patientProfilePath,
-			"subjectProfile-", 
-			sub("/", "-", comparisonDF$USUBJID), ".pdf"
+	comparisonDF <- createPatientProfileVar(
+			data = comparisonDF,
+			patientProfilePath = patientProfilePath,
+			subjectIDvar = subjectIDvar
 	)
+	comparisonDF[, subjectIDvar] <- comparisonDF$patientProfileLink
 	
-	comparisonDF$USUBJID <- with(
-			comparisonDF,
-			paste0(
-					'<a href="', patientProfilePath, 
-					'" target="_blank">', USUBJID, '</a>'
-			)
-	)
-	
-	columnsToRemove <- c("grp", "chng_type", "chng_type_diff", "Type", "Version", "patientProfilePath")
+	columnsToRemove <- c("grp", "chng_type", "chng_type_diff", "patientProfilePath", "patientProfileLink")
+	columnsToKeep <- colnames(comparisonDF)[- which(colnames(comparisonDF) %in% columnsToRemove)]
 	
 	# Make comparison table and rename columns
-	comparisonTable <- comparisonDF[,
-			c(
-					"Type", "Version",
-					colnames(comparisonDF)[
-							- which(colnames(comparisonDF) %in% columnsToRemove)
-					]
-			)
-	]
-	colnamesVars <- labelVars[colnames(comparisonTable)]	
-	
-	colnames(comparisonTable) <- c(
-			"Type", "Version",
-			colnamesVars[!is.na(colnamesVars)],
-			colnames(comparisonTable)[grepl("diff", colnames(comparisonTable))]
-	)
+	comparisonTable <- comparisonDF[, columnsToKeep]	
+	comparisonTable <- reorderColumns(
+			comparisonTable, c(
+					"Type" = 1,
+					"Version" = 2)
+	)	
+	colnames(comparisonTable) <- getLabelVar(var = colnames(comparisonTable), labelVars = labelVars)
 	
 	return(comparisonTable)
 }
-
-
-#' Extract position indices of columns in a data frame
-#' 
-#' @param df data frame
-#' @param vars character vector of columns of the data frame
-#' @author Michela Pasetto
-#' @export 
-getColumnsIdx <- function(df, vars) which(colnames(df) %in% vars)
-
-
-#' Extract position indices of columns in a data frame and subtract 1
-#' 
-#' This function is used for correspondance between R and javascript column indices 
-#' @inheritParams getColumnsIdx
-#' @author Michela Pasetto
-#' @export 
-getJavaScriptColumnsIdx <- function(...) getColumnsIdx(...) - 1
-
 
 
