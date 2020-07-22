3ea609a80342e12d71f792f7ed493dfc4134a739
Michela Pasetto <mpasetto@openanalytics.eu>
2020-07-22 18:18:06 +0200

    scrape html (ongoing)

---
+++ package/medicalMonitoring/inst/examples/scrapeHtml-example.R
@@ -0,0 +1,8 @@
+## Demographics
+demographicsHtml <- "~/Documents/Projects/Galapagos/medicalMonitoring/demoReleases/20200703/MOMP/3-3-demographics.html"
+
+## Listings
+aeListingHtml <- "~/Documents/Projects/Galapagos/medicalMonitoring/demoReleases/20200703/MOMP/5-6-adverse-events-listing.html"
+
+tableHtml <- scrapeHtml(htmlPath = aeListingHtml)
+
