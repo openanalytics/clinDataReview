640082a769c164367af3ecd82be85f19c91008bc
Michela Pasetto <mpasetto@openanalytics.eu>
2020-07-23 10:09:59 +0200

    scrape html: function with example

--- package/medicalMonitoring/R/scrapeHtml.R
+++ package/medicalMonitoring/R/scrapeHtml.R
@@ -1,24 +1,36 @@
-library(rvest)
-library(jsonlite)
 
-#' @importFrom rvest read_html html_nodes html_text
+#' Scrape html
+#' 
+#' Scrape html file(s) and retrieve the data
+#' @param htmlPath String pointing to the path
+#' where the html is stored
+#' @return The data
+#' @example inst/examples/scrapeHtml-example.R
 #' @import magrittr
+#' @importFrom rvest read_html html_nodes html_text
 #' @importFrom jsonlite fromJSON
+#' @author Michela Pasetto
+#' @export 
 scrapeHtml <- function(htmlPath) {
   
   doc <- read_html(htmlPath)
-  
   text <- doc %>% html_nodes("script[data-for]") %>% html_text
   jsonObj <- fromJSON(text)
+  
   # Extract data
   dataText <- jsonObj$x$data
   table <- t(matrix(dataText, ncol = ncol(dataText)))
+  
   # Extract colnames
   namesText <- jsonObj$x$container
-  namesText <- gsub(
-      "<table class=\"display\">\n  <thead>\n    <tr>\n      <th> </th>\n      ",
-      "",
-      namesText)
+  namesText <- gsub("<.*?>", "", namesText)
+  # unlist(strsplit(namesText, split = "<.*?>|\n|\\s+"))
+  namesText <- unlist(strsplit(namesText, split = "\n"))
+  namesText <- gsub("^\\s+", "", namesText)
+  
+  colnamesTable <- namesText[namesText != ""]
+  
+  colnames(table) <- colnamesTable
   
   return(table)
   
