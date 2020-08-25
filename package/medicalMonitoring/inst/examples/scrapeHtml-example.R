84a48efbf742bc0eab969eb837f27d5dee5a8d7a
Michela Pasetto <mpasetto@openanalytics.eu>
2020-08-25 18:00:02 +0200

    tmp comment out example

--- package/medicalMonitoring/inst/examples/scrapeHtml-example.R
+++ package/medicalMonitoring/inst/examples/scrapeHtml-example.R
@@ -1,12 +1,12 @@
 
 #' \dontrun{
 ## Demographics
-demographicsHtml <- "~/Documents/Projects/Galapagos/medicalMonitoring/demoReleases/20200703/MOMP/3-3-demographics.html"
-
-## Listings
-aeListingHtml <- "~/Documents/Projects/Galapagos/medicalMonitoring/demoReleases/20200703/MOMP/5-6-adverse-events-listing.html"
-
-tableHtml <- scrapeHtml(pathHtml = aeListingHtml)
+#demographicsHtml <- "~/Documents/Projects/Galapagos/medicalMonitoring/demoReleases/20200703/MOMP/3-3-demographics.html"
+#
+### Listings
+#aeListingHtml <- "~/Documents/Projects/Galapagos/medicalMonitoring/demoReleases/20200703/MOMP/5-6-adverse-events-listing.html"
+#
+#tableHtml <- scrapeHtml(pathHtml = aeListingHtml)
 
 #' }
 
