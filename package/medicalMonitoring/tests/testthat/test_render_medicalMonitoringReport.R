5c5bb5e9a9c9c4beb598c2cecf8d121a223a1f94
Michela Pasetto <mpasetto@openanalytics.eu>
2020-08-27 17:22:19 +0200

    tests for reading config files

---
+++ package/medicalMonitoring/tests/testthat/test_render_medicalMonitoringReport.R
@@ -0,0 +1,54 @@
+library(yaml)
+
+tmpdir <- tempdir()
+
+## General config file
+file.create("config.yaml")
+configFileGeneral <- paste0(tmpdir, "/config.yml") 
+write_yaml(
+    list(
+        study = "Study name"
+    ),
+    configFileGeneral
+)
+
+## File 1
+configFile1 <- tempfile(pattern = "config-", fileext = ".yml", tmpdir = tmpdir)
+write_yaml(
+    list(
+        reportTitle = "Title One",
+        reportTitleLevel = 2
+    ),
+    configFile1 
+)
+
+## File 2
+configFile2 <- tempfile(pattern = "config-", fileext = ".yml", tmpdir = tmpdir)
+write_yaml(
+    list(
+        reportTitle = "Title Two",
+        reportTitleLevel = 2
+    ),
+    configFile2 
+)
+configFiles <- c(configFileGeneral, configFile1, configFile2)
+configFiles <- gsub(".+[/]config(.+)", "config\\1", configFiles)
+
+test_that("Check uniqueness of report titles", {
+      
+      reportTitles <- checkReportTitles(configFiles, configDir = tmpdir)
+      expect_is(reportTitles, "character")
+      expect_length(reportTitles, 2)
+      expect_named(reportTitles, configFiles[2 : 3])
+      
+      configFilesError <- c(configFiles[2], configFiles[2])
+      expect_error(
+          checkReportTitles(configFilesError, configDir = tmpdir)
+      )
+      
+    })
+
+# Add tests for getParamsFromConfig
+# getParamsFromConfig(configFile = configFiles[1], configDir = tmpdir)
+
+file.remove(configFiles)
