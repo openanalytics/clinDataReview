9235aea271b6ac42f33e258ab086ddb018ebe994
Michela Pasetto <mpasetto@openanalytics.eu>
2020-09-23 17:08:07 +0200

    add test for formatHoverText

--- package/medicalMonitoring/tests/testthat/test_miscellaneous.R
+++ package/medicalMonitoring/tests/testthat/test_miscellaneous.R
@@ -1,2 +1,24 @@
 context("Test miscellaneous functions")
 
+test_that("Formatting of hover text", {
+      
+      x <- LETTERS
+      myLabel <- "myLabel"
+      
+      formatText <- formatHoverText(x = LETTERS, label = "myLabel")
+      myLabelFormat <- sprintf("%s: %s", myLabel, paste(LETTERS, collapse = ", "))
+      
+      expect_is(formatText, "character")
+      expect_identical(names(formatText), myLabelFormat)
+      
+      # Change default width
+      formatTextShort <- formatHoverText(x = LETTERS, label = "myLabel", width = 3)
+      expect_is(formatTextShort, "character")
+      expect_identical(names(formatText), names(formatTextShort))
+      
+      textFormat <- sprintf("%s:<br>%s", myLabel, paste(LETTERS, collapse = ",<br>"))
+      names(textFormat) <- myLabelFormat
+      expect_identical(textFormat, formatTextShort)
+      
+    })
+
