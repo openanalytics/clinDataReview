0bef18a13487501a9e4525cd48e6548d90b37f30
Michela Pasetto <mpasetto@openanalytics.eu>
2020-09-25 09:49:50 +0200

    some tests render_medicalMonitoring

--- package/medicalMonitoring/tests/testthat/test_render_medicalMonitoringReport.R
+++ package/medicalMonitoring/tests/testthat/test_render_medicalMonitoringReport.R
@@ -32,7 +32,7 @@ write_yaml(
     configFile2 
 )
 configFiles <- c(configFileGeneral, configFile1, configFile2)
-configFiles <- gsub(".+[/]config(.+)", "config\\1", configFiles)
+configFiles <- basename(configFiles)
 
 test_that("Check uniqueness of report titles", {
       
@@ -52,7 +52,76 @@ test_that("Check uniqueness of report titles", {
       
     })
 
-# Add tests for getParamsFromConfig
-# getParamsFromConfig(configFile = configFiles[1], configDir = tmpdir)
+test_that("Get path of Md from config file", {
+      
+      mdFiles <- getMdFromConfig(configFiles)
+      expect_is(mdFiles, "character")
+      expect_length(mdFiles, length(configFiles))
+      
+      configFilesShortName <- gsub("config-", "", tools::file_path_sans_ext(configFiles))
+      referenceNames <- sprintf("./interim/%s.md", c("index", configFilesShortName[configFilesShortName != "config"]))
+      expect_identical(referenceNames, mdFiles)
+      
+      # Different name for indexPath
+      mdFilesIndex <- getMdFromConfig(configFiles, indexPath = "myIndex.Rmd")
+      expect_is(mdFilesIndex, "character")
+      expect_length(mdFilesIndex, length(configFiles))
+      referenceNames <- sprintf("./interim/%s.md", c("myIndex", configFilesShortName[configFilesShortName != "config"]))
+      expect_identical(referenceNames, mdFilesIndex)
+      
+      # Different intermediateDir
+      mdFilesInterim <-getMdFromConfig(configFiles, intermediateDir = "myDir")
+      expect_is(mdFilesInterim, "character")
+      expect_length(mdFilesInterim, length(configFiles))
+      referenceNames <- sprintf("myDir/%s.md", c("index", configFilesShortName[configFilesShortName != "config"]))
+      expect_identical(referenceNames, mdFilesInterim)
+      
+    })
 
-file.remove(configFiles)
+
+test_that("Get parameters from config file", {
+      
+      filePath <- sprintf("%s/%s", tmpdir, configFiles) 
+      
+      expect_error(getParamsFromConfig(configFiles[1]))
+      
+      # General config file
+      listConfig <- getParamsFromConfig(configFiles[1], configDir = tmpdir)
+      n <- length(read_yaml(filePath[1]))
+      expect_is(listConfig, "list")
+      expect_length(listConfig, n)
+      expect_true(names(listConfig) == "study")
+      
+      # Other config file
+      listConfig <- getParamsFromConfig(configFiles[2], configDir = tmpdir)
+      n <- length(read_yaml(filePath[2])) + length(read_yaml(filePath[1]))
+      expect_is(listConfig, "list")
+      expect_length(listConfig, n)
+      expect_identical(listConfig, c(read_yaml(filePath[1]), read_yaml(filePath[2])))
+      
+    })
+
+
+test_that("Check template name in config", {
+      
+      expect_error(checkTemplatesName(configFiles, tmpdir))
+      
+      configFileTemplate <- tempfile(pattern = "config-", fileext = ".yml", tmpdir = tmpdir)
+      write_yaml(
+          list(
+              reportTitle = "Title",
+              reportTitleLevel = 2,
+              template = "divisionTemplate",
+              templatePackage = "custom"
+          ),
+          configFileTemplate 
+      )
+      configFileTemplate <- basename(configFileTemplate)     
+           
+      checkedConfig <- checkTemplatesName(configFileTemplate, configDir = tmpdir)
+      expect_is(checkedConfig, "character")
+      expect_identical(configFileTemplate, checkedConfig)
+      
+    })
+
+#file.remove(configFiles)
