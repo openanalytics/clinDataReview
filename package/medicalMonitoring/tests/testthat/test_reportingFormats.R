038e3e6e1618a29680a4062ecd66d97a767ca928
Michela Pasetto <mpasetto@openanalytics.eu>
2020-10-23 14:55:14 +0200

    test: fix in 'gitbook_medicalMonitoring_report'

--- package/medicalMonitoring/tests/testthat/test_reportingFormats.R
+++ package/medicalMonitoring/tests/testthat/test_reportingFormats.R
@@ -3,13 +3,13 @@ context("Test reporting formats")
 test_that("Format of gitbook for medical monitoring", {
       
       output <- gitbook_medicalMonitoring_report()
-      expect_is(output, list)
-      expect_legth(output, 12)
+      expect_is(output, "rmarkdown_output_format")
+      expect_length(output, 12)
       
       output <- gitbook_medicalMonitoring_report(
           split_by = 'section',
       )
-      expect_is(output, list)
+      expect_is(output, "rmarkdown_output_format")
       
       # Further testing in glpgStyle::gitbook_report
     })
