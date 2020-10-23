285d70f1b9853d21dbd7010bcabcb508fefca8a4
Michela Pasetto <mpasetto@openanalytics.eu>
2020-10-23 14:51:06 +0200

    test for 'gitbook_medicalMonitoring_report'

--- package/medicalMonitoring/tests/testthat/test_reportingFormats.R
+++ package/medicalMonitoring/tests/testthat/test_reportingFormats.R
@@ -1,2 +1,15 @@
 context("Test reporting formats")
 
+test_that("Format of gitbook for medical monitoring", {
+      
+      output <- gitbook_medicalMonitoring_report()
+      expect_is(output, list)
+      expect_legth(output, 12)
+      
+      output <- gitbook_medicalMonitoring_report(
+          split_by = 'section',
+      )
+      expect_is(output, list)
+      
+      # Further testing in glpgStyle::gitbook_report
+    })
