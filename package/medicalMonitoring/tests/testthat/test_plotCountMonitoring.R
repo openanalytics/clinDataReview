2611d58f791725f9fa640cc4a4bbe61bd80b8410
lcougnaud <laure.cougnaud@openanalytics.eu>
2020-11-05 13:59:18 +0100

    add unit tests plotCountMonitoring

--- package/medicalMonitoring/tests/testthat/test_plotCountMonitoring.R
+++ package/medicalMonitoring/tests/testthat/test_plotCountMonitoring.R
@@ -69,6 +69,37 @@ test_that("creation of count visualization for specified one parent variable is 
 			
 })
 
+test_that("child and parent variables can contain the same element", {
+			
+	data <- data.frame(
+		varParent = c("A", "A", "A", "B", "B"),
+		varChild = c("A", "b", "Total", "c", "Total"),
+		n = c(1, 2, 3, 5, 5)
+	)
+	expect_silent(
+		pl <- plotCountMonitoring(data, 
+			vars = c("varParent", "varChild"), 
+			valueVar = "n"
+		)
+	)
+	
+	plotData <- plotly_data(pl)
+	
+	# check extraction of hierarchical data
+	vars <- c("varParent", "varChild")
+	plotDataOrder <- plotData[do.call(order, plotData[, vars]), ]
+	
+	dataHierar <- data.frame(
+		hierarID = c("A-A", "A-b", "A", "B-c", "B"),
+		hierarParent = c("A", "A", "", "B", ""),
+		hierarLabel = c("A", "b", "A", "c", "B"),
+		stringsAsFactors = FALSE
+	)
+	expect_equal(plotDataOrder[, colnames(dataHierar)], dataHierar, check.attributes = FALSE)
+	
+})
+
+
 test_that("specification of color categorical variable is successful", {				
 			
 	data <- data.frame(
@@ -94,3 +125,77 @@ test_that("specification of color categorical variable is successful", {
 	
 })
 
+test_that("specification of color numerical variable with color range is successful", {				
+			
+	set.seed(123)
+	data <- data.frame(
+		varParent = sample(LETTERS[seq.int(6)], 100, replace = TRUE),
+		varChild = sample(letters[seq.int(6)], 100, replace = TRUE),
+		n = seq.int(100)
+	)
+	data <- data[!duplicated(data[, c("varParent", "varChild")]), ]
+	totalN <- with(data, tapply(n, varParent, sum))
+	data <- rbind(data,
+		data.frame(
+			varParent = names(totalN),
+			varChild = "Total",
+			n = totalN
+		)
+	)
+	
+	expect_silent(
+		pl <- plotCountMonitoring(
+			data, 
+			vars = c("varParent", "varChild"), 
+			valueVar = "n",
+			colorVar = "n"
+		)
+	)
+	
+	plData <- plotly_build(pl)$x$data[[1]]
+	colors <- plData$marker$colors
+	ids <- plData$ids
+	
+	data$ids <- with(data, paste(varParent, varChild, sep = "-"))
+	data$ids <- sub("-Total", "", data$ids)
+	colorsData <- data[match(ids, data$ids), "n"]
+	statPerColor <- tapply(colorsData, colors, range)
+	statPerColorRank <- statPerColor[order(sapply(statPerColor, min), decreasing = FALSE)]
+	isColorGroupOfStat <- all(diff(unlist(statPerColorRank)) >= 0)
+	expect_true(
+		object = isColorGroupOfStat, 
+		label = "color var doesn't represent groups of specified summary statistic"
+	)
+	
+})
+
+test_that("table is created with count visualization", {				
+			
+	data <- data.frame(
+		varParent = c("A", "A", "A", "B", "B"),
+		varChild = c("a", "b", "Total", "c", "Total"),
+		n = c(1, 2, 3, 5, 5)
+	)
+
+	expect_silent(
+		res <- plotCountMonitoring(
+			data, 
+			vars = c("varParent", "varChild"), 
+			valueVar = "n",
+			table = TRUE
+		)
+	)
+	table <- res$table
+	expect_s3_class(table, "datatables")
+	
+	tableData <- table$x$data
+	
+	tableInput <- cbind(data, hierarID = c("A-a", "A-b", "A", "B-c", "B"))
+	expect_equal(sort(colnames(dataTable)), sort(colnames(tableData)))
+	
+	expect_equal(
+		tableInput[do.call(order, tableInput), ], 
+		dataTable[do.call(order, dataTable), ]
+	)
+			
+})
