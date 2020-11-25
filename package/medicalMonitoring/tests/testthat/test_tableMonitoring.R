788585802e247d0fd22d70ca67b4b3c82c1df3a3
Michela Pasetto <mpasetto@openanalytics.eu>
2020-11-25 14:38:10 +0100

    tests for tableMonitoring

---
+++ package/medicalMonitoring/tests/testthat/test_tableMonitoring.R
@@ -0,0 +1,63 @@
+context("Test table monitoring")
+
+data <- data.frame(
+    USUBJID = c("ID1", "ID2", "ID3", "ID4"),
+    A = c(10, 12, 16, 18),
+    pathVar = "<a href=\"./path-to-report\">label</a>",
+    stringsAsFactors = FALSE
+)
+
+test_that("Table monitoring in the default application", {
+      
+      expect_silent(
+          tableMon <- 
+              tableMonitoring(
+                  data
+              )
+      )
+      expect_is(tableMon, "datatables")
+      expect_is(tableMon, "htmlwidget")
+      
+    })
+
+test_that("Have warning for 'pathVar' argument", {
+      
+      expect_warning(
+          tableMonitoring(
+              data,
+              pathVar = "varName"
+          ),
+          "Variable with path to subject profile: .* is not available"
+      )
+      
+    })
+
+test_that("Use 'pathVar' argument", {
+      
+      expect_silent(
+          tableMon <- tableMonitoring(
+              data,
+              pathVar = "pathVar"
+          )
+      )
+      expect_is(tableMon, "datatables")
+      expect_is(tableMon, "htmlwidget")
+           
+    })
+
+test_that("Use 'pathExpand' argument", {
+      
+      expect_silent(
+          tableMon <- tableMonitoring(
+              data,
+              pathVar = "pathVar",
+              pathExpand = TRUE
+          )
+      )
+      expect_is(tableMon, "datatables")
+      expect_is(tableMon, "htmlwidget")
+      
+    })
+
+# Seems 'tableMonitoring' is also tested through the other plot functionalities?
+
