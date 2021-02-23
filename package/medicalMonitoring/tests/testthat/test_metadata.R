0b13c5a1d021f610df20223d518e14a03cabb57f
Michela Pasetto <mpasetto@openanalytics.eu>
2021-02-23 15:30:40 +0100

    metadata: tests ongoing

--- package/medicalMonitoring/tests/testthat/test_metadata.R
+++ package/medicalMonitoring/tests/testthat/test_metadata.R
@@ -1,5 +1,8 @@
 context("Test metadata functionality")
 
+tmpdir <- tempdir()
+library(yaml)
+
 test_that("Check availability of metadata", {
       
       listName <- list(A = c(1, 2))
@@ -15,3 +18,59 @@ test_that("Check availability of metadata", {
       )
       
     })
+
+test_that("Get metadata", {
+      
+      expect_error(
+          getMetadata("fileNotExist"),          
+          "Metadata file does not exist."
+      )
+      tmpYamlFile <- tempfile(
+          pattern = "file", tmpdir = tempdir(), fileext = ".yml"
+      )
+      listArgs <- list(
+          pathSDTMs = "pathSDTMs",
+          pathMeMoADs = "pathMeMoADs",
+          dateTimeMeMorun = "20200101",
+          datasetInfo = list(
+              list(
+                  dataset = "ex.xpt",
+                  datetime = "20200101"
+              ),
+              list(
+                  dataset = "sl.xpt",
+                  datetime = "20200101",
+                  signatureStatus = "OK"
+              )
+          )
+      )
+      write_yaml(
+          listArgs,
+          file = tmpYamlFile
+      )
+      resMetadata <- getMetadata(filePath = tmpYamlFile)
+      expect_is(resMetadata, "list")
+      expect_equal(
+          class(resMetadata), c("medicalMonitoringMetadata", "list")
+      )
+      expect_named(resMetadata, c("pathsInfo", "datasetInfo"))
+      expect_is(resMetadata$pathsInfo, "matrix")
+      expect_is(resMetadata$datasetInfo, "data.table")
+      expect_is(resMetadata$datasetInfo, "data.frame")
+      
+      pathInfos <- resMetadata$pathsInfo
+      expect_identical(
+          rownames(pathInfos),
+          c("path SDTMs", "path MeMo ADs", "date time MeMo run")
+      )
+      
+      dfInfo <- resMetadata$datasetInfo
+      expect_identical(
+          dfInfo$signatureStatus,
+          c(NA, "OK")
+      )
+      
+    })
+
+
+
