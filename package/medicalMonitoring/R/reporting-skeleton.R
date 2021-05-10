3e3a2ee07a894252dc45b1c9bd41b7552863c8e5
Michela Pasetto <mpasetto@openanalytics.eu>
2021-05-10 17:49:17 +0200

    skeleton report ongoing (refs #25273)

---
+++ package/medicalMonitoring/R/reporting-skeleton.R
@@ -0,0 +1,46 @@
+
+reportSkeleton <- function(dirName) {
+  
+  if(! dir.exists(dirName)) stop("Directory '", dirName, "' does not exist.")
+  if(length(list.files(dirName)) > 0) warning("'", dirName, "' is not empty. Files might be overwritten.")
+  
+  # create data folder
+  dirData <- file.path(dirName, "data")
+  if(! grepl("data", list.files(dirName))) dir.create(dirData)
+  
+  # move data from clinUtils in xpt format
+  
+  # create metadata in data folder
+  createExampleMetadata(dirData)
+  
+  
+}
+
+
+#' @importFrom yaml write_yaml
+createExampleMetadata <- function(dirName) {
+  
+  fileName <- file.path(dirName, "metadata.yml")
+  
+  write_yaml(
+      list(
+          pathSDTMs = "divisionTemplate.Rmd",
+          dateTimeSDTMcreation = "20210101",
+          datasetInfo = list(
+              list(
+                  dataset = "ex.xpt",
+                  datetime = "20210101",
+                  status = "Checking ongoing"
+              ),
+              list(
+                  dataset = "sl.xpt",
+                  datetime = "20210101",
+                  status = "Checked",
+                  comments = "Nothing to report"
+              )
+          )
+      ),
+      fileName
+  )
+  
+}
