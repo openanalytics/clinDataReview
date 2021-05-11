6f3cbc6869d4c806992dd2e9c234aed6b6e3aa03
Michela Pasetto <mpasetto@openanalytics.eu>
2021-05-11 12:12:29 +0200

    report skeleton ongoing (refs #25273)

--- package/medicalMonitoring/R/reporting-skeleton.R
+++ package/medicalMonitoring/R/reporting-skeleton.R
@@ -1,19 +1,57 @@
 
+#' Create the skeleton of a report
+#' 
+#' Creates the skeleton of a report to start running the analyses.
+#' 
+#' This function is meant to get familiar with the use of the package and the 
+#' necessary files to create a report.
+#' 
+#' It will create a ready-to-use report with example data from the \code{clinUtils} 
+#' package. After getting use to the file structure, the user can 
+#' substitute the example data with 
+#' custom data sets and add specific configuration files.
+#' 
+#' @param dirName String with the path of the directory where 
+#' the skeleton should be created.
+#' @return The files to run a report are written in the specified 
+#' directory. To run the report, the user can call the 
+#' \link{\code{render_medicalMonitoringReport}}.
+#' @export 
 reportSkeleton <- function(dirName) {
   
   if(! dir.exists(dirName)) stop("Directory '", dirName, "' does not exist.")
-  if(length(list.files(dirName)) > 0) warning("'", dirName, "' is not empty. Files might be overwritten.")
+  preexistingFiles <- list.files(dirName)
+  if(length(preexistingFiles) > 0) warning("'", dirName, "' is not empty. Files might be overwritten.")
   
   # create data folder
   dirData <- file.path(dirName, "data")
-  if(! grepl("data", list.files(dirName))) dir.create(dirData)
+  if(length(preexistingFiles) == 0 || ! any(grepl("data", preexistingFiles))) dir.create(dirData)
   
-  # move data from clinUtils in xpt format
+  writeXpt(dirData)
   
-  # create metadata in data folder
   createExampleMetadata(dirData)
   
+  moveSkeletonFiles(dirName)
   
+  message("The skeleton of the report is ready!")
+  
+}
+
+
+#' @import clinUtils
+writeXpt <- function(dirName) {
+  
+  data(dataADaMCDISCP01)
+  
+  sapply(names(dataADaMCDISCP01), function(nameI) {
+        
+    dataI <- dataADaMCDISCP01[[nameI]]
+    pathI <- file.path(dirName, nameI)
+    
+    write_xpt(dataI, pathI)
+       
+  })
+ 
 }
 
 
@@ -42,5 +80,21 @@ createExampleMetadata <- function(dirName) {
       ),
       fileName
   )
+  
+}
+
+
+moveSkeletonFiles <- function(dirName) {
+  
+  skeletonFiles <- list.files(
+      system.file("skeleton", package = "medicalMonitoring"),
+      full.names = TRUE
+  )
+  file.copy(
+      from = skeletonFiles,
+      to = dirName,
+      overwrite = TRUE, recursive = TRUE
+  )
+  
   
 }
