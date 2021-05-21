adbb4999688be9c508fc1406d288fd41955ab236
Michela Pasetto <mpasetto@openanalytics.eu>
2021-05-21 15:54:55 +0200

    reporting gitbook: unit tests

--- package/clinDataReview/tests/testthat/test_reportingFormats.R
+++ package/clinDataReview/tests/testthat/test_reportingFormats.R
@@ -1,5 +1,16 @@
 context("Test reporting formats")
 
+data(iris)
+vars <- c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width")
+
+dirPlots <- file.path(tempdir(), "figures")
+dir.create(dirPlots)
+
+pathPlotBasic <- file.path(dirPlots, paste0("scatterplotIris-basic", ".png"))
+png(pathPlotBasic)
+pairs(iris[, vars])
+tmp <- dev.off()
+
 test_that("Format of gitbook for clinical data", {
       
       output <- gitbook_clinDataReview_report()
@@ -10,7 +21,7 @@ test_that("Format of gitbook for clinical data", {
           split_by = 'section',
       )
       expect_is(output, "rmarkdown_output_format")
-
+      
     })
 
 test_that("Format html report", {
@@ -21,5 +32,29 @@ test_that("Format html report", {
           res$pandoc$to,
           "html"
       )
+      
+    })
+
+test_that("Add logo", {
+      
+      
+      expect_silent(
+          res <- addLogoGitbook(
+              logo = pathPlotBasic
+          )
+      )
+      expect_is(res, "list")
+      expect_named(res, "in_header")
+      expect_is(res$in_header, "character")
+      
+    })
+
+test_that("Add logo in gitbook for clinical data", {
+      
+      expect_silent(
+          res <- gitbook_clinDataReview_report(logo = pathPlotBasic)
+      )
+      expect_is(res, "rmarkdown_output_format")
+      
       
     })
