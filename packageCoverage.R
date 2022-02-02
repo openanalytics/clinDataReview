89197e04c7ab8067af1cd471d8327da1ab2a152a
Laure Cougnaud <laure.cougnaud@openanalytics.eu>
2022-02-02 08:33:52 +0100

    remove packageCoverage (available in testPackages)

--- packageCoverage.R
+++
@@ -1,9 +0,0 @@
-library(covr)
-pc <- package_coverage(
-    path = "~/git/clinDataReview/package/clinDataReview",
-    type = "tests", quiet = FALSE, clean = FALSE
-)
-report(x = pc,
-    file = paste0("testCoverage-tests-clinDataReview",
-        packageVersion("clinDataReview"), ".html")
-)
